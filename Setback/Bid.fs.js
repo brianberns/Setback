da73f06c0f43bf46de92f4cb71dcc63ffc4d22bc
Brian Berns <brianberns@gmail.com>
2022-04-29 16:02:28 -0400

    No need to store FABLE output in source control.

--- Setback/Bid.fs.js
+++
@@ -1,5 +0,0 @@
-import { toArray } from "../Setback.Web/Client/src/.fable/fable-library.3.2.9/Seq.js";
-import { enum_type, int32_type, getEnumValues } from "../Setback.Web/Client/src/.fable/fable-library.3.2.9/Reflection.js";
-
-export const BidModule_numBids = toArray(getEnumValues(enum_type("Setback.Bid", int32_type, [["Pass", 0], ["Two", 2], ["Three", 3], ["Four", 4]]))).length;
-
