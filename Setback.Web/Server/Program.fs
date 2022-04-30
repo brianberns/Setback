e2218a5f426148ab7139db6ca6d9700e54b49ee9
Brian Berns <brianberns@gmail.com>
2022-04-30 11:14:22 -0400

    Prepare to serve files in production mode.

--- Setback.Web/Server/Program.fs
+++ Setback.Web/Server/Program.fs
@@ -1,35 +1,57 @@
 ﻿namespace Server
 
-open Suave
-open Suave.Logging
-open Suave.Operators
+module Remoting =
 
-open Fable.Remoting.Server
-open Fable.Remoting.Suave
+    open Fable.Remoting.Server
+    open Fable.Remoting.Suave
 
-open Setback.Web
+    open Setback.Web
 
-module Program =
-
-    let setbackApi =
+    let private setbackApi =
         let getActionIndex =
             Setback.Cfrm.DatabasePlayer.init "Setback.db"
         {
             GetActionIndex = getActionIndex >> async.Return
         }
 
-    let logger = Targets.create LogLevel.Info [||]
-    let webApp =
-        (Remoting.createApi()
+    let webPart =
+        Remoting.createApi()
             |> Remoting.fromValue setbackApi
-            |> Remoting.buildWebPart)
-            >=> Filters.logWithLevelStructured
-                LogLevel.Info
-                logger
-                Filters.logFormatStructured
+            |> Remoting.buildWebPart
 
-    // start the web server
+module Logging =
+
+    open Suave.Filters
+    open Suave.Logging
+
+    let webPart =
+        let logger = Targets.create LogLevel.Info [||]
+        logWithLevelStructured
+            LogLevel.Info
+            logger
+            logFormatStructured
+
+module Program =
+
+    open System.IO
+    open System.Net
+
+    open Suave
+    open Suave.Filters
+    open Suave.Operators
+
     let config =
-        { defaultConfig with
-            bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" 5000 ] }
+        {
+            defaultConfig with
+                bindings =
+                    [ HttpBinding.create HTTP IPAddress.Any 5000us ]
+                homeFolder = Some (Path.GetFullPath "./public")
+        }
+
+    let webApp =
+        choose [
+            Remoting.webPart
+            GET >=> Files.browseHome
+        ] >=> Logging.webPart
+
     startWebServer config webApp
