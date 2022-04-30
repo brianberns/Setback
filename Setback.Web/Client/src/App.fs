604fbc8401eefdfb39b15fd8a940930496a9094f
Brian Berns <brianberns@gmail.com>
2022-04-30 01:38:23 -0400

    Display cards dynamically.

--- Setback.Web/Client/src/App.fs
+++ Setback.Web/Client/src/App.fs
@@ -3,11 +3,33 @@ namespace Setback.Web.Client
 open System
 open Browser.Dom
 
+open Fable.Core.JsInterop
+
 open PlayingCards
 open Setback
 open Setback.Cfrm
 
 module App =
+
+    importAll "cardsJS/cards.css"
+    importAll "cardsJS/cards.js"
+
+    let card2C : string = importDefault "cardsJS/cards/2C.svg"
+    let card3C : string = importDefault "cardsJS/cards/3C.svg"
+    let card4C : string = importDefault "cardsJS/cards/4C.svg"
+    let myHand = document.getElementsByClassName("hand").[0] :?> Browser.Types.HTMLDivElement
+    let img2 = Image.Create()
+    img2.classList.add("card")
+    img2.src <- card2C
+    myHand.appendChild(img2) |> ignore
+    let img3 = Image.Create()
+    img3.classList.add("card")
+    img3.src <- card3C
+    myHand.appendChild(img3) |> ignore
+    let img4 = Image.Create()
+    img4.classList.add("card")
+    img4.src <- card4C
+    myHand.appendChild(img4) |> ignore
 
     // Get a reference to our button and cast the Element to an HTMLButtonElement
     let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
--- Setback.Web/Client/webpack.config.js
+++ Setback.Web/Client/webpack.config.js
@@ -22,5 +22,15 @@ module.exports = {
         }
     },
     module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ]
     }
 }
