1614a8edc6ca4d62b81392012dd02ba1d9878340
Brian Berns <brianberns@gmail.com>
2022-05-04 01:06:52 -0400

    Manage card images directly.

--- Setback.Web/Client/src/App.fs
+++ Setback.Web/Client/src/App.fs
@@ -5,60 +5,11 @@ open System
 open Browser.Dom
 open Browser.Types
 
-open Fable.Core.JsInterop
-
 open PlayingCards
 open Setback
 open Setback.Cfrm
 
-module HandDiv =
-
-    let private divMap =
-        Enum.getValues<Seat>
-            |> Seq.map (fun seat ->
-                let div =
-                    let name = Seat.toString seat
-                    let id = $"hand-{name.ToLower()}"
-                    document.getElementById(id)
-                        :?> HTMLDivElement
-                seat, div)
-            |> Map
-
-    let ofSeat seat =
-        divMap.[seat]
-
-    let addCard card (div : HTMLDivElement) =
-        card
-            |> Img.ofCard
-            |> div.appendChild
-            |> ignore
-
-module TrickDiv =
-
-    let private divMap =
-        Enum.getValues<Seat>
-            |> Seq.map (fun seat ->
-                let div =
-                    let name = Seat.toString seat
-                    let id = $"trick-{name.ToLower()}"
-                    document.getElementById(id)
-                        :?> HTMLDivElement
-                seat, div)
-            |> Map
-
-    let ofSeat seat =
-        divMap.[seat]
-
-    let addCard card (div : HTMLDivElement) =
-        card
-            |> Img.ofCard
-            |> div.appendChild
-            |> ignore
-
 module App =
-
-    importAll "cardsJS/cards.css"
-    importAll "cardsJS/cards.js"
 
     let rng = Random()
     let dealer = Seat.South
@@ -66,31 +17,14 @@ module App =
         Deck.shuffle rng
             |> AbstractOpenDeal.fromDeck dealer
 
-    for iPlayer = 0 to Seat.numSeats - 1 do
-        let seat = Seat.incr iPlayer dealer
-        let hand =
-            deal.UnplayedCards.[iPlayer]
-                |> Seq.sortDescending
-        let div = HandDiv.ofSeat seat
-        for card in hand do
-            HandDiv.addCard card div
+    let addCard card (div : HTMLDivElement) =
+        card
+            |> Img.ofCard
+            |> div.appendChild
+            |> ignore
 
-    do
-        let card = Card.fromString "AS"
-        let div = TrickDiv.ofSeat Seat.West
-        TrickDiv.addCard card div
-
-    do
-        let card = Card.fromString "AH"
-        let div = TrickDiv.ofSeat Seat.North
-        TrickDiv.addCard card div
-
-    do
-        let card = Card.fromString "AC"
-        let div = TrickDiv.ofSeat Seat.East
-        TrickDiv.addCard card div
-
-    do
-        let card = Card.fromString "AD"
-        let div = TrickDiv.ofSeat Seat.South
-        TrickDiv.addCard card div
+    let surface =
+        document.getElementById("surface")
+            :?> HTMLDivElement
+    for card in Card.allCards do
+        addCard card surface
