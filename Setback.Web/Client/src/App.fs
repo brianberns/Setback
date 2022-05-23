7fe7b6e11a8594eaf3ad81702da530e36261f760
Brian Berns <brianberns@gmail.com>
2022-05-23 15:58:40 -0400

    Work on tracking games won.

--- Setback.Web/Client/src/App.fs
+++ Setback.Web/Client/src/App.fs
@@ -2,6 +2,7 @@ namespace Setback.Web.Client
 
 open System
 
+open Browser
 open Browser.Dom
 
 open PlayingCards
@@ -84,23 +85,61 @@ module Deal =
 
 module Game =
 
+    let private ewName = "East + West"
+    let private nsName = "North + South"
+    let private teamNames = [| ewName; nsName |]
+
+    let private ewScoreElem = ~~"#ewScore"
+    let private nsScoreElem = ~~"#nsScore"
+
+    let private ewGamesWonElem = ~~"ewGamesWon"
+    let private nsGamesWonElem = ~~"nsGamesWon"
+    let private gamesWonKeys = [| "ewGamesWon"; "nsGamesWon" |]
+    let private teamGamesWonElems = [| ewGamesWonElem; nsGamesWonElem |]
+
+    /// Increments the number of games won by the given team.
+    let incrGamesWon iTeam =
+
+            // update previous count from storage
+        let key = gamesWonKeys.[iTeam]
+        let nGames =
+            WebStorage.localStorage.[key]
+                |> Option.ofObj
+                |> Option.map int
+                |> Option.defaultValue 0
+        let nGames' = nGames + 1
+        console.log($"{teamNames.[iTeam]} has won {nGames'} game(s)")
+        WebStorage.localStorage.[key] <- string nGames'
+
+            // update display
+        let gamesWonElem = teamGamesWonElems.[iTeam]
+        gamesWonElem.text(string nGames')
+
+    /// Handles the end of a game.
+    let private gameOver surface iTeam cont =
+
+            // display banner
+        let banner =
+            let text = $"{teamNames.[iTeam]} wins the game!"
+            console.log(text)
+            ~~HTMLDivElement.Create(innerText = text)
+        banner.addClass("banner")
+        banner.click(fun () ->
+            banner.remove()
+            incrGamesWon iTeam
+            cont ())
+        surface.Element.append(banner)
+
     /// Runs one new game.
     let run surface rng dealer cont =
-
-        let ewName = "East + West"
-        let nsName = "North + South"
-        let teamNames = [| ewName; nsName |]
-
-        let ewScore = ~~"#ewScore"
-        let nsScore = ~~"#nsScore"
 
         /// Runs one deal.
         let rec loop (game : Game) dealer =
 
                 // display current game score
             let absScore = Game.absoluteScore dealer game.Score
-            ewScore.text(string absScore.[0])
-            nsScore.text(string absScore.[1])
+            ewScoreElem.text(string absScore.[0])
+            nsScoreElem.text(string absScore.[1])
 
                 // run a deal
             Deal.run surface rng dealer game.Score
@@ -124,8 +163,8 @@ module Game =
                     let absScore = Game.absoluteScore dealer gameScore
                     console.log($"E+W have {absScore.[0]} point(s)")
                     console.log($"N+S have {absScore.[1]} point(s)")
-                    ewScore.text(string absScore.[0])
-                    nsScore.text(string absScore.[1])
+                    ewScoreElem.text(string absScore.[0])
+                    nsScoreElem.text(string absScore.[1])
 
                     // is the game over?
                 let winningTeamIdxOpt =
@@ -135,16 +174,8 @@ module Game =
 
                         // game is over
                     | Some iTeam ->
-
-                            // display banner
-                        let str = $"{teamNames.[iTeam]} wins the game!"
-                        console.log(str)
-                        let banner = ~~HTMLDivElement.Create(innerText = str)
-                        banner.addClass("banner")
-                        banner.click(fun () ->
-                            banner.remove()
+                        gameOver surface iTeam (fun () ->
                             cont dealer')
-                        surface.Element.append(banner)
 
                         // run another deal
                     | None ->
