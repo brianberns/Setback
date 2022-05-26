a85f8f4468fdbb3047c16fd69bda8cb64a165836
Brian Berns <brianberns@gmail.com>
2022-05-25 21:13:04 -0400

    Serve static files from ./public correctly.

--- Setback.Web/Server/WebPart.fs
+++ Setback.Web/Server/WebPart.fs
@@ -3,7 +3,6 @@
 module private Remoting =
 
     open System.IO
-    open System.Reflection
 
     open Fable.Remoting.Server
     open Fable.Remoting.Suave
@@ -11,13 +10,9 @@ module private Remoting =
     open Setback.Web
 
     /// Get bid/play for any Setback situation.
-    let private setbackApi =
+    let private setbackApi dir =
         let getActionIndex =
-            let dbPath =
-                let dir =
-                    Assembly.GetExecutingAssembly().Location
-                        |> Path.GetDirectoryName
-                Path.Combine(dir, "Setback.db")
+            let dbPath = Path.Combine(dir, "Setback.db")
             Setback.Cfrm.DatabasePlayer.init dbPath
         {
             GetActionIndex =
@@ -25,12 +20,15 @@ module private Remoting =
         }
 
     /// Build API.
-    let webPart =
+    let webPart dir =
         Remoting.createApi()
-            |> Remoting.fromValue setbackApi
+            |> Remoting.fromValue (setbackApi dir)
             |> Remoting.buildWebPart
 
 module WebPart =
+
+    open System.IO
+    open System.Reflection
 
     open Suave
     open Suave.Filters
@@ -38,8 +36,13 @@ module WebPart =
 
     /// Web part.
     let app =
-        choose [
-            Remoting.webPart
-            GET >=> Files.browseHome
-        ]
 
+        let dir =
+            Assembly.GetExecutingAssembly().Location
+                |> Path.GetDirectoryName
+        let staticPath = Path.Combine(dir, "public")
+
+        choose [
+            Remoting.webPart dir
+            GET >=> Files.browse staticPath
+        ]
