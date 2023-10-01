75070b2816958dd2ca86c2f1cc350b01cf9f37dc
Brian Berns <brianberns@gmail.com>
2023-10-01 09:56:09 -0400

    More work on session state.

--- Setback.Web/Client/src/App.fs
+++ Setback.Web/Client/src/App.fs
@@ -1,41 +1,58 @@
 namespace Setback.Web.Client
 
-open System
-
 open Browser
 
-open PlayingCards
 open Setback
 open Setback.Cfrm
 
 module Session =
 
     /// Runs a new session.
-    let run surface rng dealer =
+    let run surface sessionState =
 
         /// Plays a pair of duplicate deals.
-        let rec loop (rng : Random) dealer =
+        let rec loop sessionState =
             async {
-                    // run first game of a pair
-                let rngBefore = rng.Clone()
-                let! dealer1, nDeals1 = Game.run surface rng dealer
-                let rngAfter1 = rng.Clone()
+                if sessionState.GameParity then
 
-                    // run second game of the pair w/ duplicate deals
-                let! dealer2, nDeals2 = Game.run surface rngBefore dealer.Next
-                let rngAfter2 = rng.Clone()
+                        // run second game of pair
+                    let! _, sessionState2 = Game.run surface sessionState
 
-                    // continue with unseen RNG state
-                assert(nDeals1 <> nDeals2 || rngAfter1.State = rngAfter2.State)
-                let rngAfter, dealer' =
-                    if nDeals1 > nDeals2 then rngAfter1, dealer1
-                    else rngAfter2, dealer2
-                do! loop rngAfter dealer'
+                        // continue with unseen random state
+                    let sessionState' =
+                        { sessionState2 with
+                            GameParity = false
+                            Dealer = sessionState.Dealer.Next }
+                    do! loop sessionState'
+                else
+                        // run first game of a pair
+                    let! nDeals1, sessionState1 = Game.run surface sessionState
+
+                        // run second game of the pair w/ duplicate deals
+                    let sessionState' =
+                        { sessionState1 with
+                            RandomState = sessionState.RandomState
+                            GameParity = true
+                            Dealer = sessionState.Dealer.Next }
+                    let! nDeals2, sessionState2 = Game.run surface sessionState'
+                    assert(nDeals1 <> nDeals2
+                        || sessionState1.RandomState = sessionState2.RandomState)
+
+                        // continue with unseen random state
+                    let sessionState' =
+                        let randomState =
+                            if nDeals1 > nDeals2 then sessionState1.RandomState
+                            else sessionState2.RandomState
+                        { sessionState2 with
+                            RandomState = randomState
+                            GameParity = false
+                            Dealer = sessionState.Dealer.Next.Next }
+                    do! loop sessionState'
             }
 
         async {
             try
-                do! loop rng dealer
+                do! loop sessionState
             with ex ->
                 console.log(ex.StackTrace)
                 window.alert(ex.StackTrace)
@@ -43,38 +60,7 @@ module Session =
 
 module App =
 
-    /// Parses the given fragment for known arguments.
-    let private parseParams (parms : Types.URLSearchParams) =
-
-        let rng =
-            option {
-                let! str = parms.get("seed")
-                match UInt64.TryParse str with
-                    | true, seed -> return Random(seed)
-                    | _ -> ()
-            } |> Option.defaultWith Random
-
-        let dealer =
-            option {
-                let! str = parms.get("dealer")
-                match str.ToLower() with
-                    | "w" | "west" -> return Seat.West
-                    | "n" | "north" -> return Seat.North
-                    | "e" | "east" -> return Seat.East
-                    | "s" | "south" -> return Seat.South
-                    | _ -> ()
-            } |> Option.defaultValue Seat.South
-
-        rng, dealer
-
         // start a session when the browser is ready
     (~~document).ready(fun () ->
-
         let surface = ~~"main"
-
-            // parse params
-        let rng, dealer =
-            URL.Create(document.URL).searchParams
-                |> parseParams
-
-        Session.run surface rng dealer)
+        SessionState.get () |> Session.run surface)
--- Setback.Web/Client/src/Game.fs
+++ Setback.Web/Client/src/Game.fs
@@ -10,35 +10,51 @@ open Setback
 open Setback.Cfrm
 open Setback.Web.Client   // ugly - force AutoOpen
 
-/// State maintained on client.
-type LocalState =
+/// Session state.
+type SessionState =
     {
-        /// Number of games won by each team
+        /// Number of games won by each team.
         GamesWon : int[]   // SimpleJson doesn't support ImmutableArray
+
+        /// Number of points won by each team in the current game.
+        Scores : int[]
+
+        /// State of random number generator.
+        RandomState : uint64   // can't persist entire RNG
+
+        /// First (false) or second (true) game of a two-game pair.
+        GameParity : bool
+
+        /// Current dealer.
+        Dealer : Seat
     }
 
-module LocalState =
+module SessionState =
 
     /// Initial state.
-    let initial =
+    let private initial =
         {
             GamesWon = Array.zeroCreate Setback.numTeams
+            Scores = Array.zeroCreate Setback.numTeams
+            RandomState = Random().State
+            GameParity = false
+            Dealer = Seat.South
         }
 
-    /// Local state key.
-    let private key = "LocalState"
+    /// Local storage key.
+    let private key = "SessionState"
 
-    /// Answers the current local state.
+    /// Answers the current state.
     let get () =
         WebStorage.localStorage[key]
             |> Option.ofObj
-            |> Option.map Json.parseAs<LocalState>
+            |> Option.map Json.parseAs<SessionState>
             |> Option.defaultValue initial
 
-    /// Sets the local state.
-    let set (localState : LocalState) =
+    /// Sets the current state.
+    let set (sessionState : SessionState) =
         WebStorage.localStorage[key]
-            <- Json.serialize localState
+            <- Json.serialize sessionState
 
 // To-do:
 // * Split GameView into separate file
@@ -66,45 +82,33 @@ module Game =
             "#nsGamesWon"
         |] |> Array.map (~~)
 
-    /// Answers number of games won by the given team.
-    let private getNumGamesWon iTeam =
-        let state = LocalState.get ()
-        state.GamesWon[iTeam]
+    /// Increments the number of games won by the given team.
+    let private incrGamesWon iTeam sessionState =
 
-    /// Sets the number of games won by the given team.
-    let private setNumGamesWon iTeam (nGames : int) =
-        let state = LocalState.get ()
+            // increment count
+        let nGames = sessionState.GamesWon[iTeam] + 1
+        console.log($"{teamNames[iTeam]} has won {nGames} game(s)")
+
+            // update session state
         let gamesWon =
-            ImmutableArray(state.GamesWon)
+            ImmutableArray(sessionState.GamesWon)
                 .SetItem(iTeam, nGames)
                 .ToArray()
-        { state with GamesWon = gamesWon }
-            |> LocalState.set
-
-    /// Displays the number of games won by all teams.
-    let private displayGamesWon () =
-        for iTeam = 0 to Setback.numTeams - 1 do
-            let gamesWonElem = gamesWonElems.[iTeam]
-            let nGames = getNumGamesWon iTeam
-            gamesWonElem.text(string nGames)
-
-    /// Increments the number of games won by the given team.
-    let private incrGamesWon iTeam =
-
-            // update previous count from storage
-        let nGames = (getNumGamesWon iTeam) + 1
-        console.log($"{teamNames.[iTeam]} has won {nGames} game(s)")
-        setNumGamesWon iTeam nGames
+        let sessionState' =
+            { sessionState with GamesWon = gamesWon }
 
             // update display
-        displayGamesWon ()
+        for iTeam, nGames in Seq.indexed gamesWon do
+            gamesWonElems[iTeam].text(string nGames)
+
+        sessionState'
 
     /// Handles the end of a game.
-    let private gameOver (surface : JQueryElement) iTeam =
+    let private gameOver (surface : JQueryElement) iTeam sessionState =
 
             // display banner
         let banner =
-            let text = $"{teamNames.[iTeam]} wins the game!"
+            let text = $"{teamNames[iTeam]} wins the game!"
             console.log(text)
             ~~HTMLDivElement.Create(innerText = text)
         banner.addClass("banner")
@@ -114,21 +118,24 @@ module Game =
         Promise.create (fun resolve _reject ->
             banner.click(fun () ->
                 banner.remove()
-                incrGamesWon iTeam
-                resolve ()))
+                sessionState
+                    |> incrGamesWon iTeam
+                    |> resolve))
 
     /// Runs one new game.
-    let run surface rng dealer =
+    let run surface sessionState =
 
         /// Runs one deal.
-        let rec loop (game : Game) dealer nDeals =
+        let rec loop (game : Game) sessionState nDeals =
             async {
                     // display current game score
+                let dealer = sessionState.Dealer
                 let absScore = Game.absoluteScore dealer game.Score
                 for iTeam = 0 to Setback.numTeams - 1 do
-                    scoreElems.[iTeam].text(string absScore.[iTeam])
+                    scoreElems[iTeam].text(string absScore[iTeam])
 
                     // run a deal
+                let rng = Random(sessionState.RandomState)
                 let! deal = Deal.run surface rng dealer game.Score
 
                     // determine score of this deal
@@ -136,17 +143,17 @@ module Game =
                     deal |> AbstractOpenDeal.dealScore
                 do
                     let absScore = Game.absoluteScore dealer dealScore
-                    console.log($"E+W make {absScore.[0]} point(s)")
-                    console.log($"N+S make {absScore.[1]} point(s)")
+                    console.log($"E+W make {absScore[0]} point(s)")
+                    console.log($"N+S make {absScore[1]} point(s)")
 
                     // update game score
                 let gameScore = game.Score + dealScore
                 do
                     let absScore = Game.absoluteScore dealer gameScore
-                    console.log($"E+W have {absScore.[0]} point(s)")
-                    console.log($"N+S have {absScore.[1]} point(s)")
+                    console.log($"E+W have {absScore[0]} point(s)")
+                    console.log($"N+S have {absScore[1]} point(s)")
                     for iTeam = 0 to Setback.numTeams - 1 do
-                        scoreElems.[iTeam].text(string absScore.[iTeam])
+                        scoreElems[iTeam].text(string absScore[iTeam])
 
                     // is the game over?
                 let winningTeamIdxOpt =
@@ -157,7 +164,7 @@ module Game =
 
                         // game is over
                     | Some iTeam ->
-                        do! gameOver surface iTeam
+                        do! gameOver surface iTeam sessionState
                             |> Async.AwaitPromise
                         return dealer', nDeals'
 
@@ -170,4 +177,4 @@ module Game =
 
             // start a new game
         displayGamesWon ()
-        loop Game.zero dealer 0
+        loop Game.zero sessionState 0
