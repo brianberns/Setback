69415791e8fc1999d838c1c50d293498f2441112
Brian Berns <brianberns@gmail.com>
2023-10-02 17:47:38 -0400

    Modularized.

--- Setback.Web/Client/src/Game.fs
+++ Setback.Web/Client/src/Game.fs
@@ -3,64 +3,11 @@ namespace Setback.Web.Client
 open Browser
 
 open Fable.Core
-open Fable.SimpleJson
 
 open PlayingCards
 open Setback
 open Setback.Cfrm
 open Setback.Web.Client   // ugly - force AutoOpen
-
-/// Persistent state.
-type PersistentState =
-    {
-        /// Number of games won by each team.
-        GamesWon : AbstractScore
-
-        /// Absolute score of each team in the current game.
-        GameScore : AbstractScore
-
-        /// State of random number generator.
-        RandomState : uint64   // can't persist entire RNG
-
-        /// Current dealer.
-        Dealer : Seat
-    }
-
-module PersistentState =
-
-    /// Creates initial persistent state.
-    let private create () =
-        {
-            GamesWon = AbstractScore.zero
-            GameScore = AbstractScore.zero
-            RandomState = Random().State   // start with arbitrary seed
-            Dealer = Seat.South
-        }
-
-    /// Local storage key.
-    let private key = "PersistentState"
-
-    /// Saves the given state.
-    let save (persState : PersistentState) =
-        WebStorage.localStorage[key]
-            <- Json.serialize persState
-
-    /// Answers the current state.
-    let get () =
-        let json = WebStorage.localStorage[key] 
-        if isNull json then
-            let persState = create ()
-            save persState
-            persState
-        else
-            Json.parseAs<PersistentState>(json)
-
-type PersistentState with
-
-    /// Saves this state.
-    member persState.Save() =
-        PersistentState.save persState
-        persState
 
 // To-do:
 // * Split GameView into separate file
---
+++ Setback.Web/Client/src/PersistentState.fs
@@ -0,0 +1,61 @@
+﻿namespace Setback.Web.Client
+
+open Browser
+
+open Fable.SimpleJson
+
+open PlayingCards
+open Setback
+open Setback.Cfrm
+
+/// Persistent state.
+type PersistentState =
+    {
+        /// Number of games won by each team.
+        GamesWon : AbstractScore
+
+        /// Absolute score of each team in the current game.
+        GameScore : AbstractScore
+
+        /// State of random number generator.
+        RandomState : uint64   // can't persist entire RNG
+
+        /// Current dealer.
+        Dealer : Seat
+    }
+
+module PersistentState =
+
+    /// Creates initial persistent state.
+    let private create () =
+        {
+            GamesWon = AbstractScore.zero
+            GameScore = AbstractScore.zero
+            RandomState = Random().State   // start with arbitrary seed
+            Dealer = Seat.South
+        }
+
+    /// Local storage key.
+    let private key = "PersistentState"
+
+    /// Saves the given state.
+    let save (persState : PersistentState) =
+        WebStorage.localStorage[key]
+            <- Json.serialize persState
+
+    /// Answers the current state.
+    let get () =
+        let json = WebStorage.localStorage[key] 
+        if isNull json then
+            let persState = create ()
+            save persState
+            persState
+        else
+            Json.parseAs<PersistentState>(json)
+
+type PersistentState with
+
+    /// Saves this state.
+    member persState.Save() =
+        PersistentState.save persState
+        persState
