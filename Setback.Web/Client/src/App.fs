46a166f57820b118363fd57e8a9409a609b9830f
Brian Berns <brianberns@gmail.com>
2023-10-02 11:54:36 -0400

    Cleanup.

--- Setback.Web/Client/src/App.fs
+++ Setback.Web/Client/src/App.fs
@@ -8,53 +8,55 @@ open Setback.Cfrm
 module Session =
 
     /// Runs a new session.
-    let run surface persistentState =
+    let run surface persState =
 
         /// Plays a pair of duplicate deals.
-        let rec loop persistentState =
+        let rec loop persState =
             async {
-                match persistentState.DuplicateDealState with
+                match persState.DuplicateDealState with
 
                         // first game of a pair has already run
-                    | Some (randomState1, nDeals1) ->
-                        let persistentState1 =
-                            { persistentState with RandomState = randomState1 }   // reset RNG to repeat deals
-                        return! finish persistentState1 nDeals1
+                    | Some (rndState1, nDeals1) ->
+                        let persState1 =
+                            { persState with RandomState = rndState1 }   // reset RNG to repeat deals
+                        return! finish persState1 nDeals1
 
                         // run first game of a pair
                     | None ->
-                        let! persistentState1, nDeals1 = Game.run surface persistentState
-                        return! finish persistentState1 nDeals1
+                        let! persState1, nDeals1 = Game.run surface persState
+                        return! finish persState1 nDeals1
             }
 
-        and finish persistentState1 nDeals1 =
+        and finish persState1 nDeals1 =
             async {
                     // run second game of the pair w/ duplicate deals
-                let persistentState' =
-                    { persistentState1 with
-                        RandomState = persistentState.RandomState       // reset RNG to repeat deals
+                let persState' =
+                    { persState1 with
+                        RandomState = persState.RandomState   // reset RNG to repeat deals
                         DuplicateDealState =
-                            Some (persistentState1.RandomState, nDeals1)
-                        Dealer = persistentState.Dealer.Next }.Save()   // rotate from first dealer of game
-                let! persistentState2, nDeals2 = Game.run surface persistentState'
+                            Some (persState1.RandomState, nDeals1)
+                        Dealer = persState.Dealer.Next }      // rotate from first dealer of game
+                        .Save()
+                let! persState2, nDeals2 = Game.run surface persState'
                 assert(nDeals1 <> nDeals2
-                    || persistentState1.RandomState = persistentState2.RandomState)
+                    || persState1.RandomState = persState2.RandomState)
 
-                    // continue with unseen random state
-                let persistentState' =
-                    let randomState =
-                        if nDeals1 > nDeals2 then persistentState1.RandomState
-                        else persistentState2.RandomState
-                    { persistentState2 with
-                        RandomState = randomState
+                    // continue with unseen rnd state
+                let persState' =
+                    let rndState =
+                        if nDeals1 > nDeals2 then persState1.RandomState
+                        else persState2.RandomState
+                    { persState2 with
+                        RandomState = rndState
                         DuplicateDealState = None
-                        Dealer = persistentState.Dealer.Next.Next }.Save()
-                do! loop persistentState'
+                        Dealer = persState.Dealer.Next.Next }
+                        .Save()
+                do! loop persState'
         }
 
         async {
             try
-                do! loop persistentState
+                do! loop persState
             with ex ->
                 console.log(ex.StackTrace)
                 window.alert(ex.StackTrace)
