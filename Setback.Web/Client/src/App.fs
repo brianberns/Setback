ada28d45fd686a9c7704ea691edac94262fec01b
Brian Berns <brianberns@gmail.com>
2023-10-08 01:48:35 -0400

    Game in progress can't be part of a duplicate pair.

--- Setback.Web/Client/src/App.fs
+++ Setback.Web/Client/src/App.fs
@@ -13,31 +13,39 @@ module Session =
         /// Plays a pair of duplicate games.
         let rec loop persState =
             async {
-                    // run first game of a pair
-                console.log("Duplicate game: 1 of 2")
-                let! persState1, nDeals1 = Game.run surface persState
+                if persState.DealOpt.IsSome then
 
-                    // run second game of a pair w/ duplicate deals
-                console.log("Duplicate game: 2 of 2")
-                let persState =
-                    { persState1 with
-                        RandomState = persState.RandomState   // reset RNG to repeat deals
-                        Dealer = persState.Dealer.Next }      // rotate from first dealer of previous game
-                        .Save()
-                let! persState2, nDeals2 = Game.run surface persState
-                assert(nDeals1 <> nDeals2
-                    || persState1.RandomState = persState2.RandomState)
+                        // run game in progress
+                    console.log("Finishing game in progress")
+                    let! persState, _ = Game.run surface persState
+                    do! loop persState
 
-                    // continue with unseen random state
-                let persState =
-                    let rndState =
-                        if nDeals1 > nDeals2 then persState1.RandomState
-                        else persState2.RandomState
-                    { persState2 with
-                        RandomState = rndState
-                        Dealer = persState.Dealer.Next }
-                        .Save()
-                do! loop persState
+                else
+                        // run first game of a pair
+                    console.log("Duplicate game: 1 of 2")
+                    let! persState1, nDeals1 = Game.run surface persState
+
+                        // run second game of a pair w/ duplicate deals
+                    console.log("Duplicate game: 2 of 2")
+                    let persState =
+                        { persState1 with
+                            RandomState = persState.RandomState   // reset RNG to repeat deals
+                            Dealer = persState.Dealer.Next }      // rotate from first dealer of previous game
+                            .Save()
+                    let! persState2, nDeals2 = Game.run surface persState
+                    assert(nDeals1 <> nDeals2
+                        || persState1.RandomState = persState2.RandomState)
+
+                        // continue with unseen random state
+                    let persState =
+                        let rndState =
+                            if nDeals1 > nDeals2 then persState1.RandomState
+                            else persState2.RandomState
+                        { persState2 with
+                            RandomState = rndState
+                            Dealer = persState.Dealer.Next }
+                            .Save()
+                    do! loop persState
         }
 
         async {
