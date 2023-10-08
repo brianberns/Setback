10874499fb0ec6a0160f1a5e67dca621cc89be6f
Brian Berns <brianberns@gmail.com>
2023-10-08 11:50:01 -0400

    Backward compatibitiliy with old cookies.

--- Setback.Web/Client/src/PersistentState.fs
+++ Setback.Web/Client/src/PersistentState.fs
@@ -36,9 +36,9 @@ type PersistentState =
 module PersistentState =
 
     /// Creates initial persistent state.
-    let private create () =
+    let private create gamesWon =
         {
-            GamesWon = AbstractScore.zero
+            GamesWon = gamesWon
             GameScore = AbstractScore.zero
             RandomState = Random().State   // start with arbitrary seed
             Dealer = Seat.South
@@ -57,7 +57,20 @@ module PersistentState =
     let get () =
         let json = WebStorage.localStorage[key] 
         if isNull json then
-            let persState = create ()
+
+                // backward compatibility
+            let gamesWon =
+                let ewScore, nsScore =
+                    let parse (key : string) =
+                        let str = WebStorage.localStorage[key]
+                        WebStorage.localStorage.removeItem(key)
+                        if isNull str then 0
+                        else System.Int32.Parse(str)
+                    parse "ewGamesWon",
+                    parse "nsGamesWon"
+                AbstractScore [| ewScore; nsScore |]
+
+            let persState = create gamesWon
             save persState
             persState
         else
