092329b6b3c6d9c91c6653b5b10799f567563952
Brian Berns <brianberns@gmail.com>
2025-07-21 17:31:31 -0400

    Began work on new Killer Setback client.

---
+++ Setback.Cfrm.PlayKiller/Client.fs
@@ -0,0 +1,17 @@
+﻿namespace Setback
+
+open Avalonia.Controls
+open Avalonia.FuncUI.DSL
+
+type Model = Dummy
+
+module Client =
+
+    let init () = Dummy
+
+    let update () model = model
+
+    /// Creates a view of the given model.
+    let view model dispatch =
+        DockPanel.create [
+        ]
--- Setback.Cfrm.PlayKiller/Program.fs
+++ Setback.Cfrm.PlayKiller/Program.fs
@@ -1,7 +1,44 @@
 ﻿namespace Setback
 
+open Elmish
+
+open Avalonia
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+open Avalonia.FuncUI.Elmish
+open Avalonia.Controls.ApplicationLifetimes
+
+type MainWindow() as this =
+    inherit HostWindow(
+        Title = "Bernsrite Setback",
+        Width = 400.0,
+        Height = 100.0)
+    do
+        Program.mkSimple Client.init Client.update Client.view
+        |> Program.withHost this
+        // |> Program.withConsoleTrace
+        |> Program.runWithAvaloniaSyncDispatch ()
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add (FluentTheme())
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            let mainWindow = MainWindow()
+            desktopLifetime.MainWindow <- mainWindow
+        | _ -> ()
+
 module Program =
 
-    Killer.handshake ()
-    let wrapper = Killer.createWrapper ()
-    Killer.play wrapper |> ignore
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
