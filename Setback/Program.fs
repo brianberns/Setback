bdf0ca3d1c88c19e080c88385dc562be3a56965c
Brian Berns <brianberns@gmail.com>
2025-07-26 18:22:53 -0400

    Added temporary test driver.

---
+++ Setback/Program.fs
@@ -0,0 +1,44 @@
+﻿namespace Setback
+
+open PlayingCards
+
+module Program =
+
+    let getHand names =
+        names
+            |> Seq.map Card.fromString
+            |> set
+
+    let getHands pairs =
+        pairs
+            |> Seq.map (fun (seat, names) ->
+                seat, getHand names)
+            |> Map
+
+    let test () =
+
+        let hands =
+            [
+                Seat.West,  ["8H"; "6H"; "TD"; "8D"; "JC"; "5C"]
+                Seat.North, ["AS"; "QS"; "7S"; "TH"; "7D"; "QC"]
+                Seat.East,  ["9S"; "3S"; "2S"; "3H"; "QD"; "6D"]
+                Seat.South, ["TS"; "8S"; "5S"; "AC"; "4C"; "3C"]
+            ] |> getHands
+        let deal = OpenDeal.fromHands Seat.South hands
+
+        let deal = OpenDeal.addBid Bid.Pass deal
+        let deal = OpenDeal.addBid Bid.Pass deal
+        let deal = OpenDeal.addBid Bid.Pass deal
+        let deal = OpenDeal.addBid Bid.Two deal
+
+        let cards =
+            [
+                "AC"
+            ] |> Seq.map Card.fromString
+        let deal =
+            (deal, cards)
+                ||> Seq.fold (fun acc card ->
+                    OpenDeal.addPlay card acc)
+        printfn "%A" deal
+
+    test ()
