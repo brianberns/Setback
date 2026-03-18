c2644bb7fbafb3c46ba092f7747cd482060061bf
Brian Berns <brianberns@gmail.com>
2026-03-18 00:17:32 -0400

    Converted web server to new Deep CFR model.

--- Setback.Web/Server/WebPart.fs
+++ Setback.Web/Server/WebPart.fs
@@ -1,38 +1,13 @@
-﻿namespace Server
+﻿namespace Setback.Web
 
-module private Remoting =
+open System.IO
+open System.Reflection
 
-    open System.IO
-
-    open Fable.Remoting.Server
-    open Fable.Remoting.Suave
-
-    open Setback.Web
-
-    /// Get bid/play for any Setback situation.
-    let private setbackApi dir =
-        let getActionIndex =
-            let dbPath = Path.Combine(dir, "Setback.db")
-            Setback.Cfrm.DatabasePlayer.init dbPath
-        {
-            GetActionIndex =
-                fun key -> async { return getActionIndex key }
-        }
-
-    /// Build API.
-    let webPart dir =
-        Remoting.createApi()
-            |> Remoting.fromValue (setbackApi dir)
-            |> Remoting.buildWebPart
+open Suave
+open Suave.Filters
+open Suave.Operators
 
 module WebPart =
-
-    open System.IO
-    open System.Reflection
-
-    open Suave
-    open Suave.Filters
-    open Suave.Operators
 
     /// Web part.
     let app =
