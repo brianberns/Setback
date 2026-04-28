92c3d14a7c22bcd9bd26a04a38db8dc7c9bdbf03
Brian Berns <brianberns@gmail.com>
2026-04-27 20:03:47 -0400

    Elaborated example.

--- Setback.Experiment/Program.fs
+++ Setback.Experiment/Program.fs
@@ -1,5 +1,8 @@
 ﻿namespace Setback.PlayModel
 
+open System
+open System.IO
+
 open PlayingCards
 open Setback
 open Setback.Model
@@ -16,29 +19,54 @@ Playout.fs.js:80 East plays 9♠
 
 module Program =
 
-    let run () =
+    let runModel (model : AdvantageModel) path =
 
-        use model =
-            new AdvantageModel(1200, 5, 0.0, TorchSharp.torch.CPU)
-        model.load("AdvantageModel.pt") |> ignore
+        model.load(path : string) |> ignore
         model.eval()
 
-        let hand =
-            [ "Q♣"; "4♥"; "7♦"; "5♦"; "T♠"; "9♠" ]
-                |> Seq.map Card.fromString
-                |> set
-        let deal = ClosedDeal.create Seat.North
-        let score = Score.zero
-        let infoSet =
-            InformationSet.create Seat.East hand deal score
-        let strategy =
-            Strategy.getFromAdvantage
-                model
-                [| infoSet |]
-                |> Array.exactlyOne
-        Array.iter2 (fun bid prob ->
-            printfn $"{bid}: {prob}")
-            (Array.map Action.toBid infoSet.LegalActions)
-            (strategy.ToArray())
+        let cards =
+            [|
+                for suit in [ Suit.Spades; Suit.Diamonds; Suit.Hearts ] do
+                    for rankNum = 2 to 10 do
+                        Card(enum<Rank> rankNum, suit)
+            |]
+        [
+            for _ = 1 to 1000 do
+                let hand =
+                    set [
+                        Card.fromString "QC"
+                        yield! Array.randomShuffle cards |> Array.take 5
+                    ]
+                // printfn $"{Hand.toString hand}"
+                let deal = ClosedDeal.create Seat.North
+                let infoSet =
+                    InformationSet.create Seat.East hand deal Score.zero
+                let strategy =
+                    Strategy.getFromAdvantage
+                        model
+                        [| infoSet |]
+                        |> Array.exactlyOne
+                yield! Array.zip
+                    (Array.map Action.toBid infoSet.LegalActions)
+                    (strategy.ToArray())
+        ]
+            |> Seq.groupBy fst
+            |> Seq.map (fun (bid, group) ->
+                let avg =
+                    Seq.map snd group
+                        |> Seq.average
+                bid, avg)
+            |> Seq.iter (fun (bid, avg) ->
+                printfn $"   {bid}: {avg}")
 
-    do run ()
+    let run () =
+        use model =
+            new AdvantageModel(1200, 5, 0.0, TorchSharp.torch.CPU)
+        let dir = DirectoryInfo(@"C:\Users\brian\iCloudDrive\Setback\iter006\Training large")
+        for file in dir.GetFiles("*.pt") do
+            printfn $"{file.Name}"
+            runModel model file.FullName
+
+    do
+        Console.OutputEncoding <- Text.Encoding.UTF8
+        run ()
