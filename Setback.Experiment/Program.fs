bd80a92bfe0c5849492e46af6795987621f548b9
Brian Berns <brianberns@gmail.com>
2026-05-17 16:56:21 -0400

    Updated experiment.

--- Setback.Experiment/Program.fs
+++ Setback.Experiment/Program.fs
@@ -5,16 +5,17 @@ open Setback
 open Setback.Model
 
 (*
-E+W have 10 point(s)
-N+S have 9 point(s)
-Game.fs.js:80 Dealer is West
-Auction.fs.js:60 North bids Three
-Playout.fs.js:80 North plays J♥
-Playout.fs.js:80 North plays 7♦
-Playout.fs.js:80 North plays 4♠
-Playout.fs.js:80 North plays 4♥
-Playout.fs.js:80 North plays Q♣
-Playout.fs.js:80 North plays 6♦
+Dealer is East
+Auction.fs.js:60 South bids Three
+Auction.fs.js:60 West bids Four
+Auction.fs.js:60 North bids Pass
+Auction.fs.js:60 East bids Four
+Playout.fs.js:80 East plays J♦
+Playout.fs.js:80 East plays 9♦
+Playout.fs.js:80 East plays 3♣
+Playout.fs.js:80 East plays 3♠
+Playout.fs.js:80 East plays 8♣
+Playout.fs.js:80 East plays 9♣
 *)
 
 module Program =
@@ -22,36 +23,36 @@ module Program =
     let run () =
 
         use model =
-            new AdvantageModel(1200, 5, 0.0, TorchSharp.torch.CPU)
+            new AdvantageModel(1250, 6, 0.0, TorchSharp.torch.CPU)
         model.load("AdvantageModel.pt") |> ignore
         model.eval()
 
         let hand =
             [ 
-                "J♥"
-                "7♦"
-                "4♠"
-                "4♥"
-                "Q♣"
-                "6♦"
+                "J♦"
+                "9♦"
+                "3♣"
+                "3♠"
+                "8♣"
+                "9♣"
             ]
                 |> Seq.map Card.fromString
                 |> set
-        let deal = ClosedDeal.create Seat.West
-        for ewScore = 6 to 10 do
-            for nsScore = 6 to 10 do
-                let score = Score.ofPoints [| ewScore; nsScore |]
-                printfn $"{score}"
-                let infoSet =
-                    InformationSet.create Seat.North hand deal score
-                let strategy =
-                    Strategy.getFromAdvantage
-                        model
-                        [| infoSet |]
-                        |> Array.exactlyOne
-                Array.iter2 (fun bid prob ->
-                    printfn $"   {bid}: {prob}")
-                    (Array.map Action.toBid infoSet.LegalActions)
-                    (strategy.ToArray())
+        let deal =
+            ClosedDeal.create Seat.East
+                |> ClosedDeal.addBid Bid.Three
+                |> ClosedDeal.addBid Bid.Four
+                |> ClosedDeal.addBid Bid.Pass
+        let infoSet =
+            InformationSet.create Seat.East hand deal Score.zero
+        let strategy =
+            Strategy.getFromAdvantage
+                model
+                [| infoSet |]
+                |> Array.exactlyOne
+        Array.iter2 (fun bid prob ->
+            printfn $"   {bid}: {prob}")
+            (Array.map Action.toBid infoSet.LegalActions)
+            (strategy.ToArray())
 
     do run ()
